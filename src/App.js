1c3b240e388244a9c38266ba81c3b6244fcf45a4
evdeveloper <mushnikovz@mail.ru>
2022-02-13 23:54:44 +0300

    Получил список стран из API https://restcountries.com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios';
+import './App.css'
+import Countries from './components/Countries'
+
 
 function App() {
+  const [country, setCountry] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [countryList] = useState(10);
+
+  useEffect(()=> {
+    const getCounries = async() => {
+      setLoading(true);
+      const resp = await axios.get('https://restcountries.com/v3.1/all');
+      setCountry(resp.data);
+      setLoading(false);
+    }
+    getCounries();
+  },[]);
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App mt-5'>
+      <h1 className='text-primary mb-3 text-center'>Countries</h1>
+      <Countries countris={country} load={loading} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Countries.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Countries = ({countris, load})=> {
+  if(load) {
+    return <h2>Loading...</h2>
+  }
+  return (
+    <ul className='list-group mb-2'>
+      {
+        countris.map((country, i)=> (
+          
+          <li className='list-group-item' key={i}>
+            {country.name.common}
+            <img src={country.flags.svg} alt={country.name.common} width={25} className='ms-2' />
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
+
+
+export default Countries
